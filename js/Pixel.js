b0a307a992cea5e92ebd37ad1a8da6a8278a0b18
aidan-cary <35932149+aidan-cary@users.noreply.github.com>
2021-09-24 15:21:13 -0400

    Update Pixel.js

--- js/Pixel.js
+++ js/Pixel.js
@@ -1077,7 +1077,7 @@ Pixel.Init = function() {
 				Pixel.GetNewImage(99);
 			} else {
 				imgurImage = imgurResponse.responseJSON.gfycats[index].content_urls[0];
-				console.log(imgurImage)
+				console.log(imgurResponse.responseJSON)
 				var nsfw = false;
 				var inHistory = false;
 				if(index < 59) {
